d2ed9d484b53c3cc4eefa8d2f7d1e85676e3d7eb
arinzaa <arinzav1@gmail.com>
2024-03-07 11:14:36 +0700

    tambah fitur calcutator

--- jurnal_mod3_Arinza Aurelvia/Form1.cs
+++ jurnal_mod3_Arinza Aurelvia/Form1.cs
@@ -16,5 +16,75 @@ namespace jurnal_mod3_Arinza_Aurelvia
         {
             InitializeComponent();
         }
+        int angka = 0;
+        int hasil;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.label1.Text = this.label1.Text + this.button1.Text;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.label1.Text = this.label1.Text + this.button2.Text;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.label1.Text = this.label1.Text + this.button3.Text;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            this.label1.Text = this.label1.Text + this.button4.Text;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            this.label1.Text = this.label1.Text + this.button5.Text;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            this.label1.Text = this.label1.Text + this.button6.Text;
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            this.label1.Text = this.label1.Text + this.button7.Text;
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            this.label1.Text = this.label1.Text + this.button8.Text;
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            this.label1.Text = this.label1.Text + this.button9.Text;
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            this.label1.Text = this.label1.Text + this.button11.Text;
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            hasil = angka + int.Parse(this.label1.Text);
+            label1.Text = hasil.ToString();
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            angka = int.Parse(this.label1.Text);
+            this.label1.Text = this.label1.Text + this.button12.Text;
+            this.label1.Text = null;
+        }
     }
 }
