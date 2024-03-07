2e02b02006678eafb072ec679b8b5e4a424eee4f
arinzaa <arinzav1@gmail.com>
2024-03-07 10:25:42 +0700

    first commit

---
+++ jurnal_mod3_Arinza Aurelvia/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace jurnal_mod3_Arinza_Aurelvia
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
